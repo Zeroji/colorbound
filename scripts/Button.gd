f5974261ce3bcf985f7a44b21d3ae81df8c15d93
Zeroji <zzeroji@gmail.com>
2019-11-10 22:31:48 +0100

    Add colored buttons and animation

--- scripts/Button.gd
+++ scripts/Button.gd
@@ -3,23 +3,40 @@ extends Area2D
 
 class_name XButton
 
+const TEXTURE_SIZE:int = 16
+
 signal button_on
 signal button_off
 
 export (CS.Colors) var color = CS.Colors.yellow
+export (float) var wait_time = 0.5
 var state: bool = false
 
 func update_collision_bits():
+    print("Updating collisions, color is", color)
+    print("Wait time =", wait_time)
+    print("Bits =", CS.bits(color))
+    print(collision_layer)
     # Update layer/mask bits
     for collision_bit in CS.bits(color):
         set_collision_layer_bit(collision_bit, true)
         set_collision_mask_bit(collision_bit, true)
 
+func update_color():
+    $ColorAdd.color = color
+    $Sprite.texture.region.position.x = CS.id(color) * TEXTURE_SIZE
+    
+
 func _ready():
+    update_color()
     if not Engine.editor_hint:
         update_collision_bits()
-    $ColorAdd.color = color
-    print(collision_layer)
+        $OffTimer.wait_time = wait_time
+    print(self, collision_layer)
+
+func _process(delta):
+    if Engine.editor_hint:
+        update_color()
 
 func _on_Button_body_entered(body):
     if body is Player:
@@ -27,9 +44,18 @@ func _on_Button_body_entered(body):
             state = true
             emit_signal("button_on")
         $OffTimer.stop()
+        var seek = 0
+        if $Anim.is_playing():
+            seek = 1 - $Anim.current_animation_position / $Anim.current_animation_length
+            $Anim.stop()
+        $Anim.playback_speed = 1
+        $Anim.play("push")
+        $Anim.seek(seek)
 
 func _on_Button_body_exited(body):
     if body is Player and state:
+        $Anim.playback_speed = 1 / $OffTimer.wait_time
+        $Anim.play("release")
         $OffTimer.start()
 
 func _on_OffTimer_timeout():
