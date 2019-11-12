c2c512041823acee2b826bd2c3bcaab3dc511ece
Zeroji <zzeroji@gmail.com>
2019-11-12 19:14:52 +0100

    Add elevators (level finish point)

---
+++ scenes/Elevator.gd
@@ -0,0 +1,77 @@
+tool
+extends Node2D
+
+signal activated
+signal finished
+
+export (CS.Colors) var color = CS.Colors.red
+var opened: bool = false
+var hit_left: bool = false
+var hit_right: bool = false
+const TEXTURE_SIZE: int = 64
+
+func update_collision_bits():
+    var color_bit = CS.layer(color)
+    $Approach.set_collision_layer_bit(color_bit, true)
+    $Approach.set_collision_mask_bit(color_bit, true)
+
+func update_sprite():
+    $DoorLeft.texture.region.position.x = CS.id(color) * TEXTURE_SIZE
+    $DoorRight.texture.region.position.x = CS.id(color) * TEXTURE_SIZE
+
+func _ready():
+    if not Engine.editor_hint:
+        update_sprite()
+        update_collision_bits()
+
+func check_status():
+    if opened and hit_left and hit_right:
+        emit_signal("activated")
+        $DoorLeft.z_index += 2
+        $DoorRight.z_index += 2
+        $Animation.play_backwards("open")
+        $Animation.connect("animation_finished", self, "_on_animation_finished")
+
+func _on_animation_finished(anim):
+    if anim == "open":
+        emit_signal("finished")
+
+func _process(delta):
+    if Engine.editor_hint:
+        update_sprite()
+
+func _on_Approach_body_entered(body):
+    if body is Player and not opened:
+        opened = true
+        var pos = 0
+        if $Animation.is_playing():
+            pos = 1 - $Animation.current_animation_position / $Animation.current_animation_length
+        $Animation.play("open")
+        $Animation.seek(pos * $Animation.current_animation_length)
+
+func _on_Approach_body_exited(body):
+    if body is Player and opened:
+        opened = false
+        var pos = 0
+        if $Animation.is_playing():
+            pos = 1 - $Animation.current_animation_position / $Animation.current_animation_length
+        $Animation.play("shut")
+        $Animation.seek(pos * $Animation.current_animation_length)
+
+func _on_HitboxLeft_body_entered(body):
+    if body is Player:
+        hit_left = true
+        check_status()
+
+func _on_HitboxLeft_body_exited(body):
+    if body is Player:
+        hit_left = false
+
+func _on_HitboxRight_body_entered(body):
+    if body is Player:
+        hit_right = true
+        check_status()
+
+func _on_HitboxRight_body_exited(body):
+    if body is Player:
+        hit_right = false
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -11,6 +11,7 @@ var velocity = Vector2()
 var target_velx = 0
 var jump: float = 0
 export (CS.Colors) var color = CS.Colors.white setget set_color
+export (bool) var mobile = true
 
 func _physics_process(delta):
     var snap = Vector2(0, 20)
@@ -23,13 +24,14 @@ func _physics_process(delta):
     velocity = move_and_slide_with_snap(velocity, snap, Vector2(0, -1), true, 4, PI/3)
     
     var walk_speed = 0
-    if Input.is_action_pressed("left"):
-        walk_speed -= 1
-    if Input.is_action_pressed("right"):
-        walk_speed += 1
-    if Input.is_action_just_pressed("jump") and is_on_floor():
-        velocity.y = -JUMP_SPEED
-        jump = JUMP_TIME
+    if mobile:
+        if Input.is_action_pressed("left"):
+            walk_speed -= 1
+        if Input.is_action_pressed("right"):
+            walk_speed += 1
+        if Input.is_action_just_pressed("jump") and is_on_floor():
+            velocity.y = -JUMP_SPEED
+            jump = JUMP_TIME
     
     target_velx = lerp(target_velx, walk_speed*WALK_SPEED, 15*delta)
     velocity.x = target_velx # walk_speed*WALK_SPEED
@@ -54,3 +56,8 @@ func set_color(col):
     color = col
     $ColorAdd.set_color(color)
     $AnimatedSprite.animation = CS.name(color)
+
+func immobilize():
+    velocity.x = 0
+    target_velx = 0
+    mobile = false
