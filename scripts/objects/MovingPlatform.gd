22a45be1a68551c6014f08aea425e99d8a062cd5
Zeroji <zzeroji@gmail.com>
2019-11-22 21:59:43 +0100

    Allow moving platforms to have a linear motion

--- scripts/objects/MovingPlatform.gd
+++ scripts/objects/MovingPlatform.gd
@@ -1,9 +1,13 @@
 tool
 extends Platform
 
+enum Loop {Linear, Sin}
+
 export var seconds: float = 2
 export var loop: bool = true
-export var initial_pi: float = 0
+export (float) var initial_pi = 0
+export (Loop) var loop_type = Loop.Sin
+
 var acc: float = 0
 var origin: Vector2
 var max_acc: float
@@ -19,7 +23,14 @@ func _physics_process(delta):
         return
     acc += delta / seconds * max_acc
     acc = fmod(acc, max_acc)
-    var pos = (cos(acc-PI)+1)/2 # Map cos [-1, 1] to [0, 1]
+    var pos
+    match(loop_type):
+        Loop.Sin:
+            pos = (cos(acc-PI)+1)/2 # Map cos [-1, 1] to [0, 1]
+        Loop.Linear:
+            pos = acc/PI # Map [0, max_acc] to [0, 1]
+            if pos > 1:
+                pos = 2-pos
     $Path/Follow.unit_offset = max(0.001, min(0.999, pos))
     transform.origin = origin + $Path/Follow.position
 
