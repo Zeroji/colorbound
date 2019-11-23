eea3fafaf974226d2c94f2ca9f4f534fb2fb3b9d
Zeroji <zzeroji@gmail.com>
2019-11-23 12:13:21 +0100

    Add configurable wall height

--- scripts/objects/Wall.gd
+++ scripts/objects/Wall.gd
@@ -6,9 +6,11 @@ class_name Wall
 const TEXTURE_SIZE:int = 32
 
 export (CS.Colors) var color = CS.Colors.red
+export var height: float = 64
 
 func update_sprite():
     $Sprite.texture.region.position.x = TEXTURE_SIZE * CS.id(color)
+    $Sprite.scale.y = height / 64
     $ColorAdd.color = color
 
 func update_collision_bits():
@@ -16,6 +18,7 @@ func update_collision_bits():
     set_collision_layer_bit(CS.layer(color), true)
     for mask_bit in CS.mask(color):
         set_collision_mask_bit(mask_bit, true)
+    $CollisionShape2D.shape.extents.y = height / 2
 
 func _ready():
     if not Engine.editor_hint:
