aab18ca071e153afb9a74089bb3d24f70ca71ae1
Zeroji <zzeroji@gmail.com>
2019-11-25 21:00:54 +0100

    [IMG] Updated and merged switcher assets

--- scripts/objects/Switcher.gd
+++ scripts/objects/Switcher.gd
@@ -1,20 +1,24 @@
 tool
 extends Area2D
 
+class_name Switcher
+
+const TEXTURE_SIZE:int = 32
+
 export (CS.Colors) var color = CS.Colors.red
 
-func update_animation_state():
-    $Sprite.animation = CS.name(color)
+func update_sprite():
+    $Sprite.texture.region.position.x = TEXTURE_SIZE * CS.id(color)
     $ColorAdd.color = color
 
 func _ready():
     if not Engine.editor_hint:
-        update_animation_state()
+        update_sprite()
 
 # warning-ignore:unused_argument
 func _process(delta):
     if Engine.editor_hint:
-        update_animation_state()
+        update_sprite()
 
 func _on_Switcher_body_entered(body):
     if body is Player:
